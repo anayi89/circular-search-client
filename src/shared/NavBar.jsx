7fe8a877f2da3c5853c803c7705ddf4bcfc5264d
Iyana Garry <it.garry@gmail.com>
2025-04-11 21:36:47 -0400

    Fixed errors with classes and onClick attributes.

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -1,9 +1,21 @@
+import React from "react";
+
 function NavBar() {
+  const openNav = () => {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementsByClassName("fa-solid fa-bars")[0].style.display = "none";
+  };
+
+  const closeNav = () => {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementsByClassName("fa-solid fa-bars")[0].style.display = "inline-block";
+  };
+
     return (
       <nav className="nav_menu">
-        <div id="myNav" class="overlay">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <div class="overlay-content">
+        <div id="myNav" className="overlay">
+            <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
+            <div className="overlay-content">
                 <a href="#">Home</a>
                 <a href="submit.html">Submit Circulars</a>
                 <a href="search.html">Search Circulars</a>
@@ -12,7 +24,7 @@ function NavBar() {
             </div>      
           </div>
           
-        <i class="fa-solid fa-bars" onclick="openNav()"></i>
+        <i className="fa-solid fa-bars" onClick={openNav}></i>
       </nav>
     )
 }
